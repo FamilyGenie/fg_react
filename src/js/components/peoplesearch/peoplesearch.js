648908aadbc60e83a760929be3813a518f220b6e
Erty Seidohl <erty@codecraftschool.com>
2016-12-13 17:23:09 -0700

    Add People Search Page (without routing)

---
+++ src/js/components/peoplesearch/peoplesearch.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { connect } from "react-redux"
+
+import PeopleSearchLineItem from './peoplesearch-lineitem';
+
+@connect((store) => {
+  console.log("in PeopleSearch.js, @connect");
+  return {
+    user: store.user.user,
+    userFetched: store.user.fetched,
+    people: store.people.people,
+  };
+})
+export default class PeopleSearch extends React.Component {
+	render = () => {
+        const { people } = this.props;
+
+        const mappedPeople = people.map(person =>
+            <PeopleSearchLineItem person={person} key={person._id}/>
+        );
+
+        return (<div>
+    		<div class="container">
+                <h1>Family Members</h1>
+            </div>
+            <div>
+                <div class="row">
+                    <div class="col-xs-2 title bold can-click">
+                        First Name
+                    </div>
+                    <div class="col-xs-2 title bold can-click">
+                        Middle Name
+                    </div>
+                </div>
+            </div>
+            <div>
+            	{mappedPeople}
+            </div>
+        </div>);
+    }
+}
