55b1eb2a2c0bdcbd826a764b5f964efe371db815
Erty Seidohl <erty@codecraftschool.com>
2016-12-30 13:57:25 -0600

    Clean up pairBondRelsReducer

--- src/js/reducers/pairBondRelsReducer.js
+++ src/js/reducers/pairBondRelsReducer.js
@@ -1,72 +1,108 @@
-export default function reducer(state={
+export default function reducer(
+	state = {
 		pairBondRels: [],
 		fetching: false,
 		error: null,
-	}, action) {
-
+	},
+	action = ""
+) {
 		switch (action.type) {
 			case "FETCH_PAIRBONDRELS": {
-				return {...state, fetching: true}
+				return {
+					...state,
+					fetching: true
+				};
 			}
 			case "FETCH_PAIRBONDRELS_REJECTED": {
-				return {...state, fetching: false, error: action.payload}
+				return {
+					...state,
+					fetching: false,
+					error: action.payload
+				};
 			}
 			case "FETCH_PAIRBONDRELS_FULFILLED": {
 				return {
 					...state,
 					fetching: false,
 					pairBondRels: action.payload,
-				}
+				};
 			}
 			case "UPDATE_PAIRBONDREL": {
-				return {...state, fetching: true}
+				return {
+					...state,
+					fetching: true
+				};
 			}
 			case "UPDATE_PAIRBONDREL_FULFILLED": {
 				// todo: throw error on invalid field???
 				const newPairBondRel = action.payload;
-				const oldPairBondRelIndex = state.pairBondRels.findIndex(pairBondRel => pairBondRel._id === newPairBondRel._id);
-				const newPairBondRels = [...state.pairBondRels.slice(0, oldPairBondRelIndex), newPairBondRel, ...state.pairBondRels.slice(oldPairBondRelIndex+1)];
+				const oldPairBondRelIndex = state.pairBondRels.findIndex(
+					pairBondRel => pairBondRel._id === newPairBondRel._id
+				);
+				const newPairBondRels = [
+					...state.pairBondRels.slice(0, oldPairBondRelIndex),
+					newPairBondRel,
+					...state.pairBondRels.slice(oldPairBondRelIndex+1)
+				];
 
 				return {
 					...state,
 					fetching: false,
 					pairBondRels: newPairBondRels,
-				}
+				};
 			}
 			case "UPDATE_PAIRBONDREL_REJECTED": {
-				return {...state, fetching: false, error: action.payload}
-			}
-			case "CREATE_PAIRBONDREL": {
-				return {...state, fetching: true}
-			}
-			case "CREATE_PAIRBONDREL_FULFILLED": {
-				const newObject = action.payload;
-				const newArray = state.pairBondRels;
-				newArray.push(newObject);
 				return {
 					...state,
 					fetching: false,
-					pairBondRels: newArray,
-				}
+					error: action.payload
+				};
+			}
+			case "CREATE_PAIRBONDREL": {
+				return {
+					...state,
+					fetching: true
+				};
+			}
+			case "CREATE_PAIRBONDREL_FULFILLED": {
+				return {
+					...state,
+					fetching: false,
+					pairBondRels: [
+						...state.pairBondRels,
+						action.payload
+					],
+				};
 			}
 			case "CREATE_PAIRBONDREL_REJECTED": {
-				return {...state, fetching: false, error: action.payload}
+				return {
+					...state,
+					fetching: false,
+					error: action.payload
+				};
 			}
 			case "DELETE_PAIRBONDREL": {
-				return {...state, fetching: true}
+				return {
+					...state,
+					fetching: true
+				};
 			}
 			case "DELETE_PAIRBONDREL_FULFILLED": {
 				// todo: throw error on invalid field???
-				// the delete person api returns all people, so just set the newPeople array to the payload that is returned
-				const newArray = action.payload;
+				// the delete person api returns all people, so just set the
+				// newPeople array to the payload that is returned
 				return {
 					...state,
 					fetching: false,
-					pairBondRels: newArray,
-				}
+					pairBondRels: action.payload,
+				};
 			}
 			case "DELETE_PAIRBONDREL_REJECTED": {
-				return {...state, fetching: false, error: action.payload}
+				return {
+					...state,
+					fetching: false,
+					error: action.payload
+				};
 			}
 		}
 		return state
