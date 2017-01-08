6eb93e2d933b64a7b5a00f273bb24b68ab73fabc
joshuaforman <joshuajforman@gmail.com>
2017-01-08 15:08:29 -0700

    adding navbar to Layout.js

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -18,9 +18,56 @@ export default class Layout extends React.Component {
 		this.props.dispatch(fetchPeople());
 	}
 
+	logIn = () => {
+
+	}
+
+	logOut = () => {
+
+	}
+
 	render() {
-		return <div>
-			{this.props.children}
+		return (
+		<div>
+			<nav class="navbar navbar-default navbar-fixed-top">
+				<ul id="logoname" class="nav navbar-nav navbar-left">
+					<li>
+						<img class="navbarleft lampimg" src="../assets/images/lamp.png"/>
+					</li>
+				</ul>
+				<ul class="nav navbar-nav navbar-left">
+					<li class="navbar-li-padding">
+						<div class="navbar-header">
+						  <a routerLink="/" class="navbar-brand">
+							Family Genie <sup>&trade;</sup>
+						  </a>
+						</div>
+					</li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					{/*
+					<li class="navbar-li-padding" [class.active]="router.isActive('/peoplesearch', true)">
+					*/}
+					<li class="navbar-li-padding">
+						<a class="navbarright" routerLink="/peoplesearch">FAMILY LIST</a>
+					</li>
+					{/*
+					<li *ngIf="this.authService.isLoggedIn()" class="navbar-li-padding" >
+					*/}
+					<li class="navbar-li-padding">
+						<a class="navbarright" onClick={this.logOut}>LOG OUT</a>
+					</li>
+					{/*
+					<li *ngIf="!this.authService.isLoggedIn()" class="navbar-li-padding" >*/}
+					<li class="navbar-li-padding" >
+						<a class="navbarright" onClick={this.logIn}>LOG IN</a>
+					</li>
+				</ul>
+			</nav>
+			<div>
+				{this.props.children}
+			</div>
 		</div>
+		);
 	}
 }
