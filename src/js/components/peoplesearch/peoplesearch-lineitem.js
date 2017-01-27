612b821526b09f8973f2f2db6679a00ad283cdca
Edward Vetter-Drake <edward.vetterdrake@gmail.com>
2017-01-26 17:06:14 -0700

    fixed peoplesearch-lineitem render method

--- src/js/components/peoplesearch/peoplesearch-lineitem.js
+++ src/js/components/peoplesearch/peoplesearch-lineitem.js
@@ -38,37 +38,42 @@ export default class PeopleSearchLineItem extends React.Component {
     }
   }
 
-	render = () => (
-		<div id="person-div">
-			<div id="name-date">
-				<div class="custom-input">
-					<p class="person-text">
-						{this.props.person.fName}
-					</p>
-					<p class="person-text">
-						{this.props.person.mName}
-					</p>
-					<p class="person-text">
-						{this.props.person.lName}
-					</p>
-				</div>
-				<div class="date-div">
-					<p class="person-text">
-            {this.birthDate()}
-          </p>
-				</div>
-			</div>
-			<div id="details-map">
-				<i
-					class="fa fa-pencil-square-o button2"
-					onClick={this.openDetails}>
-				</i>
-				<i
-					class="fa fa-sitemap button2"
-					onClick={this.openMap}
-				>
-			</i>
-			</div>
-		</div>
-	);
+	render = () => {
+
+    const { person } = this.props;
+
+    return (
+      <div id="person-div">
+        <div id="name-date">
+          <div class="custom-input">
+            <p class="person-text">
+              {person.fName}
+            </p>
+            <p class="person-text">
+              {person.mName}
+            </p>
+            <p class="person-text">
+              {person.lName}
+            </p>
+          </div>
+          <div class="date-div">
+            <p class="person-text">
+              {this.birthDate()}
+            </p>
+          </div>
+        </div>
+        <div id="details-map">
+          <i
+            class="fa fa-pencil-square-o button2"
+            onClick={this.openDetails}>
+          </i>
+          <i
+            class="fa fa-sitemap button2"
+            onClick={this.openMap}
+          >
+        </i>
+        </div>
+      </div>
+    )
+	};
 }
