7b7cc1ca0ada8e598218caf95f8ffc920a9b4295
joshuaforman <joshuajforman@gmail.com>
2017-01-28 13:45:50 -0700

    Fixing reverting to accidentally deleted code.

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -49,36 +49,36 @@ export default class Layout extends React.Component {
 		return (
 		<div>
 			<nav class="navbar navbar-default">
- -			  <div class="container-fluid">
- -			    <div class="navbar-header">
- -			      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
- -			        <span class="sr-only">Toggle navigation</span>
- -			        <span class="icon-bar"></span>
- -			        <span class="icon-bar"></span>
- -			        <span class="icon-bar"></span>
- -			      </button>
- -						<a class="navbar-brand" onClick={this.goToPeopleSearch}>
- -						Family Genie <sup>&trade;</sup>
- -						</a>
- -			    </div>
- -			    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
- -
- -						<ul class="nav navbar-nav navbar-right">
- -		          <li>
- -		            <a class="navbarright" onClick={this.goToPeopleSearch}>FAMILY LIST</a>
- -		          </li>
- -		          <li>
- -		            <a class="navbarright" onClick={this.goToPeopleStaged}>STAGED LIST</a>
- -		          </li>
- -		          <li>
- -		            <a class="navbarright" onClick={this.logOut}>LOG OUT</a>
- -		          </li>
- -		          <li>
- -		            <a class="navbarright" onClick={this.logIn}>LOG IN</a>
- -		          </li>
- -		        </ul>
- -			    </div>
- -			  </div>
+<div class="container-fluid">
+			    <div class="navbar-header">
+			      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			      </button>
+						<a class="navbar-brand" onClick={this.goToPeopleSearch}>
+						Family Genie <sup>&trade;</sup>
+						</a>
+			    </div>
+			    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+						<ul class="nav navbar-nav navbar-right">
+		          <li>
+		            <a class="navbarright" onClick={this.goToPeopleSearch}>FAMILY LIST</a>
+		          </li>
+		          <li>
+		            <a class="navbarright" onClick={this.goToPeopleStaged}>STAGED LIST</a>
+		          </li>
+		          <li>
+		            <a class="navbarright" onClick={this.logOut}>LOG OUT</a>
+		          </li>
+		          <li>
+		            <a class="navbarright" onClick={this.logIn}>LOG IN</a>
+		          </li>
+		        </ul>
+			    </div>
+			  </div>
 			</nav>
 			<div>
 				{this.props.children}
