d8d788321dd67411ba55866a4680769839addf80
Edward Vetter-Drake <edward.vetterdrake@gmail.com>
2017-02-07 11:16:12 -0700

    initial chronology

--- src/js/components/chronology/chronology.js
+++ src/js/components/chronology/chronology.js
@@ -5,13 +5,7 @@ import moment from 'moment';
 import ChronologyLineItem from './chronology-lineitem';
 
 @connect((store, ownProps) => {
-  console.log('in chronology@connect with: ', store, ownProps)
   return {
-    /*
-     * events: store.events.events.filter(function(e) {
-     *   return e.user_id === store.user.user.id;
-     * }),
-     */
     events: store.events.events,
   }
 })
@@ -24,6 +18,10 @@ export default class Chronology extends React.Component {
     };
   }
 
+  componentDidMount = () => {
+    this.sortEvents('date');
+  }
+
   sortEvents = (sortType) => {
     this.setState({reverse : !this.state.reverse})
     sortType = sortType || '';
@@ -31,26 +29,22 @@ export default class Chronology extends React.Component {
     if (this.state.reverse) {
       if (sortType === 'date') {
         sortedEvents = this.props.events.sort(function(a, b) {
-          console.log('in date acending')
-          return moment(b.eventDate) - moment(a.eventDate);
+          return moment(b.eventDate.substr(0,10), 'YYYY-MM-DD') - moment(a.eventDate.substr(0,10), 'YYYY-MM-DD');
         });
       }
       else if (sortType === 'type') {
-        console.log('in sort type ascending')
         sortedEvents = this.props.events.sort(function(a, b) {
           return (b.eventType > a.eventType ? 1 : -1);
         })
       }
       /*
        * else if (sortType === 'place') {
-       *   console.log('in sort place')
        *   sortedEvents = this.props.events.sort(function(a, b) {
        *     return b.eventType - a.eventType;
        *   })
        * }
        * else {
        *   sortedEvents = this.props.events.sort(function(a, b) {
-       *     console.log('in default', a, b)
        *     return moment(a.eventDate) - moment(b.eventDate);
        *   });
        * }
@@ -59,25 +53,21 @@ export default class Chronology extends React.Component {
     else {
         if (sortType === 'date') {
           sortedEvents = this.props.events.sort(function(a, b) {
-            console.log('in date decending')
-            return moment(a.eventDate) - moment(b.eventDate);
+            return moment(a.eventDate.substr(0,10), 'YYYY-MM-DD') - moment(b.eventDate.substr(0,10), 'YYYY-MM-DD');
           });
         }
         else if (sortType === 'type') {
-        console.log('in sort type descending')
           sortedEvents = this.props.events.sort(function(a, b) {
           return (b.eventType < a.eventType ? 1 : -1);
           })
         }
         /*
          * else if (sortType === 'place') {
-         * console.log('in sort place')
          *   sortedEvents = this.props.events.sort(function(a, b) {
          *     return a.eventType - b.eventType;
          *   })
          * }
          * else {  
-         *     console.log('in default 2')
          *   sortedEvents = this.props.events.sort(function(a, b) {
          *     return moment(a.eventDate) - moment(b.eventDate);
          *   });
@@ -88,7 +78,6 @@ export default class Chronology extends React.Component {
     var mappedEvents = sortedEvents.map(event => 
       <ChronologyLineItem event={event} eventId={event._id} key={event._id}/>
     );
-    console.log(mappedEvents)
     this.setState({mappedEvents: mappedEvents});
     return mappedEvents
   }
@@ -99,7 +88,6 @@ export default class Chronology extends React.Component {
     const { events } = this.props;
     const { reverse }  = this.state;
 
-    console.log('hutansuhetandi',this.mappedEvents)
     
     if(events) {
       return(<div>
@@ -110,9 +98,9 @@ export default class Chronology extends React.Component {
           <div class="col-xs-1"></div>
           {/*using the arrow function in the onClick allows for passing in parameters, in the case of reverseSort, it prevents it from being called during the render method.*/} 
           <div class="col-xs-2"><span onClick={() => this.sortEvents('date')}> Date </span></div>
-          <div class="col-xs-3"> Person <span onClick={() => this.reverseSort()}>***</span></div>
+          <div class="col-xs-3"> Person </div>
           <div class="col-xs-2"><span onClick={() => this.sortEvents('type')}> Type </span></div>
-          <div class="col-xs-2"> Place <span onClick={() => this.reverseSort()}>***</span></div>
+          <div class="col-xs-2"> Place </div>
           {this.state.mappedEvents}
         </div>
         
