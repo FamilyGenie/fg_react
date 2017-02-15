13548b1f7241c45119efc1145f65f8b6e6190e7c
joshuaforman <joshuajforman@gmail.com>
2017-02-15 11:43:36 -0700

    separated out a axiosConfig file for login only

--- src/js/actions/actionFunctions.js
+++ src/js/actions/actionFunctions.js
@@ -2,6 +2,18 @@ import cookie from 'react-cookie';
 
 export function getAxiosConfig() {
 	return {
-		headers: {'x-access-token': cookie.load('fg-access-token')}
+		headers: {
+			'x-access-token': cookie.load('fg-access-token')
+		}
+	}
+}
+
+export function getAxiosConfigForLogin() {
+	return {
+		timeout: 5000,
+		headers: {
+			'Content-Type': 'application/json'
+			// 'Authorization': undefined
+		}
 	}
 }
