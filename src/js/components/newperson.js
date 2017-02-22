5b2a36f8b1b02d852ee0b101f8d52252911c9842
Edward Vetter-Drake <edward.vetterdrake@gmail.com>
2017-02-22 11:08:07 -0700

    bad merge fix

--- src/js/components/newperson.js
+++ src/js/components/newperson.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import PeopleDetailsLineItem from './peopledetails/peopledetails-lineitem';
+import EventLineItemEdit from './peopledetails/event-lineitem-edit';
+import ParentalRelLineItemEdit from './peopledetails/parentalrel-lineitem-edit';
 import { updatePerson } from '../actions/peopleActions';
 import { updateEvent } from '../actions/eventsActions';
 import { updateParentalRel } from '../actions/parentalRelsActions';
-import { closeNewPersonModal, deleteNewPerson, saveNewPerson } from '../actions/modalActions';
+import { closeNewPersonModal } from '../actions/modalActions';
 
-
-
-/* the following is the code that needs to be inserted into the parent component render method where you will call this modal to open.
-You can look in the peoplesearch component for an example of a component that calls this component
-
+/* the following is the code that needs to be inserted into the parent component where you will call this modal to open.
+ 
   <Modal
         isOpen={modalIsOpen}
         contentLabel="Modal"
@@ -18,11 +18,11 @@ You can look in the peoplesearch component for an example of a component that ca
       >
         <NewPerson/>
       </Modal>
-
 */
 
 @connect(
   (store, ownProps) => {
+  console.log('in newperson@connect with: ', store);
     return {
       person: store.people.people.find(function(s) {
         return (store.modal.newPerson.id === s._id)
@@ -41,18 +41,12 @@ You can look in the peoplesearch component for an example of a component that ca
       closeNewPersonModal: () => {
         dispatch(closeNewPersonModal());
       },
-      deleteNewPerson: (_id) => {
-        dispatch(deleteNewPerson());
-      },
-      saveNewPerson: () => {
-        dispatch(saveNewPerson());
-      },
     }
   }
 )
 
 export default class NewPerson extends React.Component {
-
+ 
   closeModal = () => {
     // This is validation for the contents of the modal. The user must either delete the person or enter the required information.
     if (!this.props.events[0].eventDate) { // the first record should be the newly created Birth record, might need some validation here.
@@ -68,64 +62,38 @@ export default class NewPerson extends React.Component {
     }
   }
 
-  deleteNewPerson = () => {
-    console.log("inside delete new person")
-    this.props.deleteNewPerson(this.props.newPerson.id, evt.value);
-
-    if (this.props.closeNewPersonModal) {
-      this.props.closeModal();
-    }
-  }
-  savePerson = () => {
-    this.props.saveNewPerson(this.props.newPerson.id, evt.value);
-    if (this.props.closeNewPersonModal) {
-      this.props.closeModal();
-    }
-  }
-
   render = () => {
 
     const { person, events, parents, modalIsOpen } = this.props;
 
     // events must be mapped to the lineItem, and cannot be passed in individually, not sure why this happens, leaving it for now
-    const mappedEvents = events.map(event =>
-      <CompactEvent event={event} key={event._id}/>
+    const mappedEvents = events.map(event => 
+      <EventLineItemEdit event={event} key={event._id}/>
     )
 
     const mappedParents = parents.map(parentalRel =>
-    <CompactParentalRel parentalRel={parentalRel} key={parentalRel._id}/>
+    <ParentalRelLineItemEdit parentalRel={parentalRel} key={parentalRel._id}/>
     );
 
 
-      return(
-      <div>
-        <div class="modalClose2">
-          <i class="fa fa-window-close-o fa-2x" aria-hidden="true" onClick={this.closeModal}></i>
-        </div>
-        <div class="modalH">
-            New Person
-        </div>
-        <div class="buffer-modal">
-        </div>
-        <CompactPeopleDetails person={person} key={person._id}/>
-          {mappedEvents}
-          {mappedParents}
-        <div class="event-delete-modal">
-					<button
-						type="button"
-						class="btn btn-default modal-delete"
-						onClick={this.savePerson}
-					>
-						Save
-					</button>
-					<button
-						type="button"
-						class="btn btn-default modal-delete"
-						onClick={this.deleteNewPerson}
-					>
-						Cancel
-					</button>
-				</div>
+      return(<div>
+          <h3> New Person </h3>
+          
+          <PeopleDetailsLineItem person={person} key={person._id}/>
+
+          <div class="container">
+            <h3> Events </h3>
+            <div class="col-xs-12">
+              {mappedEvents}
+            </div>
+          </div>
+          <div class="container">
+            <h3> Parents </h3>
+            <div class="col-xs-12">
+              {mappedParents}
+            </div>
+          </div>
+          <button onClick={this.closeModal}> Close </button>
       </div>);
   }
 }
