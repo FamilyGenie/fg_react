4a39b6d1df4734e6eaeca6b8d4b099337679abb9
joshuaforman <joshuajforman@gmail.com>
2017-03-29 18:43:41 -0600

    took out the headers for the mega map

--- src/js/components/familymap/megamap.js
+++ src/js/components/familymap/megamap.js
@@ -91,56 +91,6 @@ export default class MegaMap extends React.Component {
 		});
 	}
 
-    // this function will be called when the user hits the button to subtract a year and then re-draw the map
-	subtractYear = () => {
-		var vDate = moment(this.state.vDate).subtract(1,'year').format('YYYY-MM-DD');
-		var vStarAge = parseInt(this.state.starAge) - 1;
-		// also set the state variable
-		this.setState({
-			vDate: vDate,
-			starAge: vStarAge
-		});
-		// this.drawMap(this.mapStartX);
-	}
-
-    // this function will be called when the user hits the button to add a year and then re-draw the map
-	addYear = () => {
-		var vDate = moment(this.state.vDate).add(1,'year').format('YYYY-MM-DD');
-		var vStarAge = parseInt(this.state.starAge) + 1;
-		// also set the state variable
-		this.setState({
-			vDate: vDate,
-			starAge: vStarAge
-		});
-		// this.drawMap(this.mapStartX);
-	}
-
-	// this function is to make the input box for the age a "controlled component". Good information about it here: https://facebook.github.io/react/docs/forms.html
-	onAgeChange = (evt) => {
-		var star = this.getPersonById(this.props.star_id);
-		var vDate = moment(star.birthDate.replace(/T.+/, ''), 'YYYY-MM-DD').add(evt.target.value,'y').format('YYYY-MM-DD');
-
-		this.setState({
-			vDate: vDate,
-			starAge: evt.target.value,
-			mapArray: []
-		});
-		// this.drawMap(this.mapStartX);
-	}
-
-	toggleLegend = () => {
-		if(this.state.legendShowing === false) {
-			$("#legend").css({"display": "flex"});
-			$("#mainMap").css({"min-height": "1100px"});
-			this.setState({legendShowing: true});
-		}
-		if (this.state.legendShowing === true) {
-			$("#legend").css({"display": "none"});
-			$("#mainMap").css({"min-height": "800px"});
-			this.setState({legendShowing: false});
-		}
-	}
-
 	zoomOut = () => {
 		var zoom = this.state.zoom - 1;
 		this.setState({
@@ -161,8 +111,13 @@ export default class MegaMap extends React.Component {
 
 	createMapArray = () => {
 		var star = this.getPersonById('57d639cdd9a9db9e36353c9a');
-		var newComp = this.createMapComponent(star._id, moment(star.birthDate.replace(/T.+/, ''), 'YYYY-MM-DD').add(18,'y').format('YYYY-MM-DD'), .75);
+		var newComp = this.createMapComponent(star._id, moment(star.birthDate.replace(/T.+/, ''), 'YYYY-MM-DD').add(18,'y').format('YYYY-MM-DD'), .25);
 		this.state.mapArray.push(newComp);
+
+		star = this.getPersonById('57d31c66b189048209d53d6f');
+		newComp = this.createMapComponent(star._id, moment(star.birthDate.replace(/T.+/, ''), 'YYYY-MM-DD').add(18,'y').format('YYYY-MM-DD'), .75);
+		this.state.mapArray.push(newComp);
+
 		this.setState({
 			mapArray: this.state.mapArray
 		})
@@ -181,42 +136,8 @@ export default class MegaMap extends React.Component {
 
 		return(
 			<div class="mainDiv">
-				<div id="legend">
-					<Legend toggleLegend={this.toggleLegend}/>
-				</div>
-				<div class="mainMap" id="mainMapHead">
-					<div class="mapHeader">
-						<div class="dateToggle">
-							<div class="mapDate">
-								<div class="mapDateContents">
-									<p class="mapDateHeader">Family Map As Of:</p>
-									<p class="mapDateText">{this.state.vDate}</p>
-								</div>
-								<div class="starAgeLegend">
-									<p class="starAge">Star's Age:</p>
-									<input
-										id="ageInput"
-										class="form-control ageInput"
-										type="text"
-										value={this.state.starAge}
-										onChange={this.onAgeChange}
-									/>
-								<p class="starAge2">{this.state.starAge}</p>
-								<button type="button" class="btn btn-default btn-Legend" onClick={this.toggleLegend}>Legend</button>
-								</div>
-							</div>
-							<div class="mapArrow">
-								<i class="fa fa-arrow-circle-up buttonSize2 button2" onClick={this.addYear}></i>
-								<i class="fa fa-arrow-circle-down buttonSize2 button2" onClick={this.subtractYear.bind(this)}></i>
-							</div>
-							<div class="zoom">
-								<p class="zoomHead">Zoom</p>
-								<i class="fa fa-plus buttonSize3 button2" onClick={this.zoomIn}></i>
-								<i class="fa fa-minus buttonSize3 button2" onClick={this.zoomOut}></i>
-							</div>
-						</div>
-						<h1 class="map-header">{this.state.fullName}'s Family Map </h1>
-					</div>
+				<div>
+					<h1 class="map-header">{this.state.fullName}'s Family Map </h1>
 				</div>
 				{this.state.mapArray}
 			</div>
